0c1b777b5d010ec2cac014216bdf9edd8bc033ad
Ngowda94 <Nagraj94>
2022-07-04 22:05:09 +0530

    Added: code for claculating employee wages using switch case statement

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -1,23 +1,28 @@
 package com.bridgelabz;
 
 public class EmpWageBuilder {
-    static int IS_FULL_TIME = 1;
-    static int IS_PART_TIME = 2;
+
+    final static int IS_FULL_TIME = 1;
+    final static int IS_PART_TIME = 2;
     static int EMP_RATE_PER_HRS =20;
 
     public static void main(String[] args) {
         int empHrs;
         int dailyWages;
         int empCheck = (int) (Math.floor(Math.random()*10)%3);
-        if (empCheck == IS_FULL_TIME) {
-            System.out.println("employee is present for full time");
-            empHrs = 8;
-        } else if (empCheck == IS_PART_TIME) {
-            System.out.println("employee is present for half time");
-            empHrs = 4;
-        } else{
-            System.out.println("employee is absent");
-            empHrs = 0;
+        switch (empCheck){
+            case IS_FULL_TIME:
+                System.out.println("employee is present for full time");
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                System.out.println("employee is present for half time");
+                empHrs = 4;
+                break;
+            default:
+                System.out.println("employee is absent");
+                empHrs = 0;
+                break;
         }
         dailyWages = empHrs*EMP_RATE_PER_HRS;
         System.out.println("employee daily wages: "+dailyWages);
