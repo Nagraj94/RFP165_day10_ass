fb6596fc72e5f4cbb5655e28dee25026f9a92cd0
Ngowda94 <Nagraj94>
2022-07-04 22:29:12 +0530

    Added: code for calculating employee wage per month

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -5,26 +5,33 @@ public class EmpWageBuilder {
     final static int IS_FULL_TIME = 1;
     final static int IS_PART_TIME = 2;
     static int EMP_RATE_PER_HRS =20;
+    static int DAYS_PER_MONTH = 20;
 
     public static void main(String[] args) {
-        int empHrs;
-        int dailyWages;
-        int empCheck = (int) (Math.floor(Math.random()*10)%3);
-        switch (empCheck){
-            case IS_FULL_TIME:
-                System.out.println("employee is present for full time");
-                empHrs = 8;
-                break;
-            case IS_PART_TIME:
-                System.out.println("employee is present for half time");
-                empHrs = 4;
-                break;
-            default:
-                System.out.println("employee is absent");
-                empHrs = 0;
-                break;
+        int empHrs = 0;
+        int dailyWages = 0;
+        int wagePerMonth = 0;
+        for (int day=1; day<=DAYS_PER_MONTH;day++){
+            System.out.print("day "+day);
+            int empCheck = (int) (Math.floor(Math.random()*10)%3);
+            switch (empCheck){
+                case IS_FULL_TIME:
+                    System.out.println(" employee is present for full time");
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    System.out.println(" employee is present for half time");
+                    empHrs = 4;
+                    break;
+                default:
+                    System.out.println(" employee is absent");
+                    empHrs = 0;
+                    break;
+            }
+            dailyWages = empHrs*EMP_RATE_PER_HRS;
+            wagePerMonth += dailyWages;
         }
-        dailyWages = empHrs*EMP_RATE_PER_HRS;
-        System.out.println("employee daily wages: "+dailyWages);
+        System.out.println(" ");
+        System.out.println("employee wages per month is : "+wagePerMonth);
     }
 }
